4730663c0e36e0386ecbb9b7c297934e5f200374
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-05-31 12:43:35 -0600

    Widgets de Spotify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NoimagePipe } from './pipes/noimage.pipe';
 import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { DomspotifyPipe } from './pipes/domspotify.pipe';
 
 
 @NgModule({
@@ -22,7 +24,9 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     NavbarComponent,
     NoimagePipe,
     TarjetasComponent,
-    LoadingComponent
+    LoadingComponent,
+    DomseguroPipe,
+    DomspotifyPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/domspotify.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer  } from '@angular/platform-browser';
+@Pipe({
+  name: 'domspotify'
+})
+export class DomspotifyPipe implements PipeTransform {
+
+  constructor( private domSanitizer:DomSanitizer ){ }
+
+  transform( value: string ): any {
+    const url = 'https://open.spotify.com/embed/?uri=';
+    return this.domSanitizer.bypassSecurityTrustResourceUrl( url + value );
+  }
+
+}
